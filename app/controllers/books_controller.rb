514284b02e6eab643650e3efe67d09139935afe4
fukushiro sumire <fukushiro-sumire@outlook.jp>
2016-06-22 11:14:52 +0900

    12章変更点7終了後

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -62,6 +62,7 @@ class BooksController < ApplicationController
   end
 
   def search
+   @search_word = params["search"]["title"]
    @books = Book.where(title: params["search"]["title"])
    render :index
   end
