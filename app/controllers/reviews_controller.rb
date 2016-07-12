864c909a540b7879b89edcc2eb07d0988b6b7c67
fukushiro sumire <fukushiro-sumire@outlook.jp>
2016-07-12 16:54:19 +0900

    変更点B終了

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,7 +24,7 @@ class ReviewsController < ApplicationController
   # POST /reviews
   # POST /reviews.json
   def create
-    @review = Review.new(review_params)
+      @review = Review.new(review_params)
 
     respond_to do |format|
       if @review.save
@@ -69,6 +69,6 @@ class ReviewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:book_id, :body)
+      params.require(:review).permit(:book_id, :body, :user_id)
     end
 end
